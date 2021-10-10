e2b4d25458bdfbc590fe4d0470b726174b5094db
Arda Onat <ardonat97@hotmail.com>
2021-10-10 15:34:03 +0300

    Add ApiClient protocol.

---
+++ Sources/CoreModule/Network Components/ApiClient.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Arda Onat on 10.10.2021.
+//
+
+import Foundation
+
+public protocol ApiClient {
+    func request<T>(_ request: Request, queue: DispatchQueue, completion: @escaping (Result<T, NetworkError>) -> ()) where T : Decodable
+}
