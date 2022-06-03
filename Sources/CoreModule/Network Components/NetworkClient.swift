8b2658d5cda7dd71be67a30139fbf34ae6cb7f0f
Arda Onat <arda.onat@global.com>
2022-06-03 14:34:11 +0100

    Updateeeeee

--- Sources/CoreModule/Network Components/NetworkClient.swift
+++ Sources/CoreModule/Network Components/NetworkClient.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
+public struct SuccessResult<T: Decodable> {
+    public let decodedResponse: T
+    public let httpURLResponse: HTTPURLResponse
+    
+    public init(decodedResponse: T, httpURLResponse: HTTPURLResponse) {
+        self.decodedResponse = decodedResponse
+        self.httpURLResponse = httpURLResponse
+    }
+}
+
 public protocol NetworkClient {
-    func request<T>(_ request: Request, queue: DispatchQueue, completion: @escaping (Result<T, NetworkError>) -> ()) where T : Decodable
+    func request<T>(_ request: CoreModule.Request, queue: DispatchQueue, completion: @escaping (Result<SuccessResult<T>, NetworkError>) -> ())
 }
