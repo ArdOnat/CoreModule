df40238ad22893a8f95bbb02d6bf6f39a37e32e0
Arda Onat <arda.onat@ceng.deu.edu.tr>
2023-05-08 17:46:00 +0100

    Quick string fixes.

--- Sources/CoreModule/Network Components/NetworkEnvironment.swift
+++ Sources/CoreModule/Network Components/NetworkEnvironment.swift
@@ -1,4 +1,4 @@
-/// For each api environment that the app will interac,t a new NetworkEnvironment struct, class or enum that conforms to this protocol should be created.
+/// For each api environment that the app will interact a new NetworkEnvironment struct, class or enum that conforms to this protocol should be created.
 /// If enums are used, each network environment can define and store multiple api urls  for an single ApiEnvironment. (prod, test etc.)
 /** Example:
  enum WeatherForecastNetworkEnvironment: NetworkEnvironment {
