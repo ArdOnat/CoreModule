cbd007accfb6233718fc495b71bac44beaf1ea8b
Arda Onat <arda.onat@ceng.deu.edu.tr>
2024-05-21 22:56:43 +0300

    Make request parameters settable.

--- Sources/CoreModule/Network Components/Request.swift
+++ Sources/CoreModule/Network Components/Request.swift
@@ -4,9 +4,9 @@ public typealias Parameters = [String: Any]
 /// Enum, structs that conform to this protocol should be created in App and sent to ApiClient module.
 public protocol Request {
     var apiEnvironment: ApiEnvironment { get set }
-    var path: String { get }
+    var path: String { get set }
     var httpMethod: HTTPMethods { get }
-    var urlParameters: Parameters? { get }
-    var bodyParameters: Parameters? { get }
-    var httpHeaders: HTTPHeaders? { get }
+    var urlParameters: Parameters? { get set }
+    var bodyParameters: Parameters? { get set }
+    var httpHeaders: HTTPHeaders? { get set }
 }
