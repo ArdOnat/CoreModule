be2bd8e718e2dbad822f14cc56eb71d0bd29d124
Arda Onat <arda.onat@ceng.deu.edu.tr>
2024-05-21 23:11:13 +0300

    Change Request to be a struct instead of a protocol.

--- Sources/CoreModule/Network Components/Request.swift
+++ Sources/CoreModule/Network Components/Request.swift
@@ -1,12 +1,20 @@
 public typealias HTTPHeaders = [String: String]
 public typealias Parameters = [String: Any]
 
-/// Enum, structs that conform to this protocol should be created in App and sent to ApiClient module.
-public protocol Request {
-    var apiEnvironment: ApiEnvironment { get set }
-    var path: String { get set }
-    var httpMethod: HTTPMethods { get }
-    var urlParameters: Parameters? { get set }
-    var bodyParameters: Parameters? { get set }
-    var httpHeaders: HTTPHeaders? { get set }
+public struct Request {
+    public let apiEnvironment: ApiEnvironment
+    public let path: String
+    public let httpMethod: HTTPMethods
+    public let urlParameters: Parameters?
+    public let bodyParameters: Parameters?
+    public let httpHeaders: HTTPHeaders?
+
+    public init(apiEnvironment: ApiEnvironment, path: String, httpMethod: HTTPMethods, urlParameters: Parameters?, bodyParameters: Parameters?, httpHeaders: HTTPHeaders?) {
+        self.apiEnvironment = apiEnvironment
+        self.path = path
+        self.httpMethod = httpMethod
+        self.urlParameters = urlParameters
+        self.bodyParameters = bodyParameters
+        self.httpHeaders = httpHeaders
+    }
 }
