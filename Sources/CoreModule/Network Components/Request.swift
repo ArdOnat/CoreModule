d4eead2927f6e205343fce699cf65fc6c6bc7f82
Arda Onat <arda.onat@ceng.deu.edu.tr>
2024-05-21 23:24:18 +0300

    Make values inside request var.

--- Sources/CoreModule/Network Components/Request.swift
+++ Sources/CoreModule/Network Components/Request.swift
@@ -2,19 +2,10 @@ public typealias HTTPHeaders = [String: String]
 public typealias Parameters = [String: Any]
 
 public struct Request {
-    public let apiEnvironment: ApiEnvironment
-    public let path: String
-    public let httpMethod: HTTPMethods
-    public let urlParameters: Parameters?
-    public let bodyParameters: Parameters?
-    public let httpHeaders: HTTPHeaders?
-
-    public init(apiEnvironment: ApiEnvironment, path: String, httpMethod: HTTPMethods, urlParameters: Parameters?, bodyParameters: Parameters?, httpHeaders: HTTPHeaders?) {
-        self.apiEnvironment = apiEnvironment
-        self.path = path
-        self.httpMethod = httpMethod
-        self.urlParameters = urlParameters
-        self.bodyParameters = bodyParameters
-        self.httpHeaders = httpHeaders
-    }
+    public var apiEnvironment: ApiEnvironment
+    public var path: String
+    public var httpMethod: HTTPMethods
+    public var urlParameters: Parameters?
+    public var bodyParameters: Parameters?
+    public var httpHeaders: HTTPHeaders?
 }
