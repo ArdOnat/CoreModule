dca2d52acb010cbc04d1ee0321e048a4b3f7cdbe
Arda Onat <arda.onat@ceng.deu.edu.tr>
2024-05-21 23:28:06 +0300

    Add public init.

--- Sources/CoreModule/Network Components/Request.swift
+++ Sources/CoreModule/Network Components/Request.swift
@@ -8,4 +8,13 @@ public struct Request {
     public var urlParameters: Parameters?
     public var bodyParameters: Parameters?
     public var httpHeaders: HTTPHeaders?
+
+    public init(apiEnvironment: ApiEnvironment, path: String, httpMethod: HTTPMethods, urlParameters: Parameters? = nil, bodyParameters: Parameters? = nil, httpHeaders: HTTPHeaders? = nil) {
+        self.apiEnvironment = apiEnvironment
+        self.path = path
+        self.httpMethod = httpMethod
+        self.urlParameters = urlParameters
+        self.bodyParameters = bodyParameters
+        self.httpHeaders = httpHeaders
+    }
 }
